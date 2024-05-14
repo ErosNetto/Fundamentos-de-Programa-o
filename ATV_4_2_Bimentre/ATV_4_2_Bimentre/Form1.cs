93d9e064bb245c64d21bd73755f3883aaffbcb92
Eros Netto Antunes <113219307+ErosNetto@users.noreply.github.com>
2024-05-14 20:03:52 -0300

    teste

--- ATV_4_2_Bimentre/ATV_4_2_Bimentre/Form1.cs
+++ ATV_4_2_Bimentre/ATV_4_2_Bimentre/Form1.cs
@@ -16,30 +16,27 @@ namespace ATV_4_2_Bimentre
                 cliques++;
                 labelCliques.Text = cliques.ToString();
 
-                if (cliques == 7)
-                {
-                    labelMensagem.Text = "Faltam apenas mais 3\r\n";
-                    labelCliques.ForeColor = Color.Yellow;
-                    labelMensagem.ForeColor = Color.Yellow;
-                }
-
-                if (cliques == 8)
-                {
-                    labelMensagem.Text = "Faltam apenas mais 2\r\n";
-                    labelCliques.ForeColor = Color.DarkOrange;
-                    labelMensagem.ForeColor = Color.DarkOrange;
-                }
-
-                if (cliques == 9) {
-                    labelMensagem.Text = "Faltam apenas mais 1\r\n";
-                    labelCliques.ForeColor = Color.Red;
-                    labelMensagem.ForeColor = Color.Red;
-                }
-
-                if (cliques == 10) {
-                    labelMensagem.Text = "Acabou!\r\n";
-                    labelCliques.ForeColor = Color.Green;
-                    labelMensagem.ForeColor = Color.Green;
+                switch (cliques) {
+                    case 7:
+                        labelMensagem.Text = "Faltam apenas mais 3\r\n";
+                        labelCliques.ForeColor = Color.Yellow;
+                        labelMensagem.ForeColor = Color.Yellow;
+                        break;
+                    case 8:
+                        labelMensagem.Text = "Faltam apenas mais 2\r\n";
+                        labelCliques.ForeColor = Color.DarkOrange;
+                        labelMensagem.ForeColor = Color.DarkOrange;
+                        break;
+                    case 9:
+                        labelMensagem.Text = "Faltam apenas mais 1\r\n";
+                        labelCliques.ForeColor = Color.Red;
+                        labelMensagem.ForeColor = Color.Red;
+                        break;
+                    case 10:
+                        labelMensagem.Text = "Acabou!\r\n";
+                        labelCliques.ForeColor = Color.Green;
+                        labelMensagem.ForeColor = Color.Green;
+                        break;
                 }
             }
         }
